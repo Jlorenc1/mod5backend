226bb5249e6d5bbf925f1a62f9765498508ef25c
jlorenc1 <joshua.lorenc@icloud.com>
2020-08-31 13:49:07 -0700

    test

---
+++ db/migrate/20200831202139_create_games.rb
@@ -0,0 +1,13 @@
+class CreateGames < ActiveRecord::Migration[6.0]
+  def change
+    create_table :games do |t|
+      t.string :user
+      t.string :created_at
+      t.string :score
+      t.string :completion_time
+      t.string :accuracy
+
+      t.timestamps
+    end
+  end
+end
